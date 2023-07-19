9d8aa1b5fb8c458a1493bfe0c64d1d89a4ae32fc
Jihee Ryu <chrisjihee@etri.re.kr>
2023-07-19 17:02:35 +0900

    Initial commit

---
+++ setup-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+conda create -n WiseData-2023.07 python=3.10 -y
+conda activate WiseData-2023.07
+pip install -r requirements.txt
+pip list
+
+rm -rf chrisbase chrislab
+git clone git@github.com:chrisjihee/chrisbase.git
+git clone git@github.com:chrisjihee/chrislab.git
+pip install --editable chrisbase
+pip install --editable chrislab
