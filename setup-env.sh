a3ee56552a6c3b0839b969501a3afea52323a64f
Jihee Ryu <chrisjihee@etri.re.kr>
2023-07-21 11:25:36 +0900

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -10,8 +10,8 @@ git clone git@github.com:chrisjihee/chrislab.git
 pip install --editable chrisbase
 pip install --editable chrislab
 
-pip download --no-binary :all: --no-deps Wikipedia-API==0.6.0
 rm -rf Wikipedia-API-*
+pip download --no-binary :all: --no-deps Wikipedia-API==0.6.0
 tar zxf *.tar.gz
 rm *.tar.gz
-pip install --editable Wikipedia-API-0.6.0
+\
