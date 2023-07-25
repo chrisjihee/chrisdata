3c11c7e918954deabd03044681e778b49ba6d0e7
Jihee Ryu <chrisjihee@etri.re.kr>
2023-07-25 18:41:49 +0900

    Update minor

--- setup-env.sh
+++ setup-env.sh
@@ -4,18 +4,21 @@ conda activate WiseData-2023.07
 pip install -r requirements.txt
 pip list
 
+# chrisbase, chrislab
 rm -rf chrisbase chrislab
 git clone git@github.com:chrisjihee/chrisbase.git
 git clone git@github.com:chrisjihee/chrislab.git
 pip install --editable chrisbase
 pip install --editable chrislab
 
+# Wikipedia-API
 rm -rf Wikipedia-API-*
 pip download --no-binary :all: --no-deps Wikipedia-API==0.6.0
 tar zxf *.tar.gz
 rm *.tar.gz
 pip install --editable Wikipedia-API-0.6.0
 
+# MongoDB
 mkdir mongodb
 cd mongodb
 mkdir data log
