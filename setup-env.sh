160635c29f0a07c9846a40cfb77885d8a4b1ea0a
Jihee Ryu <chrisjihee@etri.re.kr>
2023-09-08 13:42:53 +0900

    Update setup-env.sh, requirements.txt

--- setup-env.sh
+++ setup-env.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-# Python
+# for running program
 conda create -n WiseData-2023.08 python=3.10 -y
 conda activate WiseData-2023.08
 pip install -r requirements.txt
 pip list
+
+# for developing library
+rm -rf chrisbase* chrislab*
+git clone https://github.com/chrisjihee/chrisbase.git
+git clone https://github.com/chrisjihee/chrislab.git
+pip install --editable chrisbase
+pip install --editable chrislab
 
 # MongoDB
 mkdir mongodb
