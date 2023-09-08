5d82c20431c505238472abadccdaa4115c358986
Jihee Ryu <chrisjihee@etri.re.kr>
2023-09-08 13:56:07 +0900

    Updating parse()

--- parse_wikidata.py
+++ parse_wikidata.py
@@ -1,4 +1,5 @@
 import logging
+import gzip
 
 from chrisbase.data import AppTyper
 
@@ -8,7 +9,15 @@ app = AppTyper()
 
 @app.command()
 def parse():
-    pass
+    num_line = 100
+    with gzip.open("/fed/Wikidata/latest-truthy-nt-gz/latest-truthy.nt.gz") as f:
+        n = 0
+        for line in f:
+            print(line)
+            n += 1
+            if n > num_line:
+                break
+        print(f"f={f}")
 
 
 if __name__ == "__main__":
