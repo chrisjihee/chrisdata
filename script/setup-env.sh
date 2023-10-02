b244ae480057d3a9dce245162b848ec7ab745b65
Jihee Ryu <chrisjihee@etri.re.kr>
2023-10-02 14:12:39 +0900

    Update setup-env.sh

--- script/setup-env.sh
+++ script/setup-env.sh
@@ -29,7 +29,7 @@ cd elasticsearch8 || exit
 if [ "$(uname)" = "Linux" ]; then
   wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.10.2-linux-x86_64.tar.gz
 elif [ "$(uname)" = "Darwin" ]; then
-  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.10.2-darwin-x86_64.tar.gz
+  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.10.2-darwin-aarch64.tar.gz
 fi
 tar zxvf elasticsearch-*.tar.gz --strip-components 1
 cd ..
