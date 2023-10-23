19962c56470ac8cbc2636a479818cb1c79678898
Jihee Ryu <chrisjihee@etri.re.kr>
2023-10-23 17:02:51 +0900

    Update setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -1,13 +1,13 @@
 # for running program
-conda create -n WiseData-2023.08 python=3.10 -y
-conda activate WiseData-2023.08
+mamba create -n WiseData-2023.08 python=3.10 -y
+mamba activate WiseData-2023.08
 pip install -r requirements.txt
 pip list
 
 # for developing library
 rm -rf chrisbase* chrislab*
-git clone https://github.com/chrisjihee/chrisbase.git
-git clone https://github.com/chrisjihee/chrislab.git
+git clone git@github.com:chrisjihee/chrisbase.git
+git clone git@github.com:chrisjihee/chrislab.git
 pip install --editable chrisbase
 pip install --editable chrislab
 
