ce2cf86e3cdeeaf386c1b78e1e33d5cad2971001
Jihee Ryu <chrisjihee@etri.re.kr>
2023-12-11 13:20:19 +0900

    Update convert_wikipedia.py

--- convert_wikipedia.py
+++ convert_wikipedia.py
@@ -1,32 +1,37 @@
-from pathlib import Path
 import logging
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from pathlib import Path
 
 import jsonlines
 from dataclasses_json import DataClassJsonMixin
 
 from chrisbase.io import num_lines, LoggingFormat, configure_unit_logger
 from chrisbase.util import mute_tqdm_cls
-from parse_wikidata import WikidataUnit
 
 logger = logging.getLogger(__name__)
 configure_unit_logger(fmt=LoggingFormat.CHECK_12)
 
 
 @dataclass
-class WikidataFreebaseID(DataClassJsonMixin):
-    wikidata_id: str
-    freebase_id: str
-    label1: str
-    label2: str
-    title1: str
-    title2: str
-    descr1: str
-    descr2: str
+class WikipediaProcessResult(DataClassJsonMixin):
+    _id: int
+    query: str
+    title: str | None = None
+    page_id: int | None = None
+    section_list: list = field(default_factory=list)
+    passage_list: list = field(default_factory=list)
+
+
+@dataclass
+class WikipediaDefinition(DataClassJsonMixin):
+    page_id: int
+    query: str
+    title: str
+    definition: str
 
 
 input_file = Path('input/wikimedia/wikipedia-20230920-crawl-kowiki.jsonl')
-output_file = Path('input/wikimedia/wikidata-20230920-definition.jsonl')
+output_file = Path('input/wikimedia/wikipedia-20230920-definition.jsonl')
 tqdm = mute_tqdm_cls()
 
 logger.info(f"Input file: {input_file}")
@@ -40,23 +45,18 @@ with jsonlines.open(input_file) as reader, output_file.open("w") as writer:
     for i, x in enumerate(progress):
         if i > 0 and i % interval == 0:
             logger.info(progress)
-        print(x)
-        exit(1)
-        # unit = WikidataUnit.from_dict(x)
-        # if unit.type == "item":
-        #     for claim in unit.claims:
-        #         if claim['property'] == "P646" and claim['datavalue']['type'] == "string":
-        #             freebase_id = claim['datavalue']['value']
-        #             writer.writelines([
-        #                 WikidataFreebaseID(
-        #                     wikidata_id=unit._id,
-        #                     freebase_id=freebase_id,
-        #                     label1=unit.label1,
-        #                     label2=unit.label2,
-        #                     title1=unit.title1,
-        #                     title2=unit.title2,
-        #                     descr1=unit.descr1,
-        #                     descr2=unit.descr2,
-        #                 ).to_json(ensure_ascii=False),
-        #                 '\n'
-        #             ])
+        doc: WikipediaProcessResult = WikipediaProcessResult.from_dict(x)
+        doc.title = doc.title.strip() if doc.title else ""
+        if doc.page_id and doc.title and doc.section_list:
+            definition = doc.section_list[0][-1]
+            writer.writelines([
+                WikipediaDefinition(
+                    page_id=doc.page_id,
+                    query=doc.query,
+                    title=doc.title,
+                    definition=definition,
+                ).to_json(ensure_ascii=False),
+                '\n'
+            ])
+            writer.flush()
+    logger.info(progress)
