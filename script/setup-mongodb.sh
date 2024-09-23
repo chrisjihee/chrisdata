f7397169018cd3a9f3a12dba8338cd129eb456fd
Jihee Ryu <chrisjihee@etri.re.kr>
2024-09-23 19:40:09 +0900

    Update setup-mongodb.sh: for ubuntu2204

--- script/setup-mongodb.sh
+++ script/setup-mongodb.sh
@@ -3,9 +3,9 @@ mkdir mongodb
 cd mongodb || exit
 mkdir data log
 if [ "$(uname)" = "Linux" ]; then
-  aria2c https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-7.0.1.tgz
+  aria2c https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2204-7.0.14.tgz
 elif [ "$(uname)" = "Darwin" ]; then
-  aria2c https://fastdl.mongodb.org/osx/mongodb-macos-arm64-7.0.1.tgz
+  aria2c https://fastdl.mongodb.org/osx/mongodb-macos-arm64-7.0.14.tgz
 fi
 tar zxvf mongodb-*.tgz --strip-components 1
 cd ..
