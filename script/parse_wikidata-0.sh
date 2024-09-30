dec60b54c48521c5f4afb35fda470f8206dc6424
Jihee Ryu <chrisjihee@etri.re.kr>
2024-09-30 17:42:40 +0900

    Update script

--- script/parse_wikidata-0.sh
+++ script/parse_wikidata-0.sh
@@ -1,2 +1,3 @@
+python -m chrisdata.cli wikidata parse --output-table-reset --no-export
 python -m chrisdata.cli wikidata parse --input-inter 100 --input-limit 10000 --output-table-reset
 python -m chrisdata.cli wikidata parse --input-inter 100 --input-limit 20000
