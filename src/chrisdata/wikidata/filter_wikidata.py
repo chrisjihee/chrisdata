59fd219e6c78bc3a2d1379d020e2c20b3832a16e
Jihee Ryu <chrisjihee@etri.re.kr>
2024-10-03 20:59:29 +0900

    Remove ExtraOption, filter_many2(), filter_many3()

--- src/chrisdata/wikidata/filter_wikidata.py
+++ src/chrisdata/wikidata/filter_wikidata.py
@@ -1,12 +1,10 @@
-import multiprocessing
-from concurrent.futures import ProcessPoolExecutor
 from typing import Iterable
 
 import typer
 
 from chrisbase.data import FileStreamer, MongoStreamer
 from chrisbase.data import InputOption, OutputOption, FileOption, TableOption
-from chrisbase.data import JobTimer, ProjectEnv, OptionData
+from chrisbase.data import JobTimer, ProjectEnv
 from chrisbase.io import LoggingFormat, new_path
 from chrisbase.util import mute_tqdm_cls
 from chrisdata.wikidata import *
@@ -14,47 +12,21 @@ from chrisdata.wikidata import *
 logger = logging.getLogger(__name__)
 
 
-@dataclass
-class ExtraOption(OptionData):
-    processor: str = field(default="filter_many1")
-
-
-def filter_one(x: dict) -> WikidataUnit | None:
-    subject: WikidataUnit = WikidataUnit.from_dict(x)
-    if subject.type != "item":
+def filter_one(x: dict) -> str | None:
+    if x['type'] != "item":
         return None
-    if subject.title1 and len(subject.title1) > 0:
-        return subject['_id']
+    if x['title1'] and len(x['title1']) > 0:
+        return f"{x['_id']}\n"
     else:
         return None
 
 
-def filter_many1(item: dict | Iterable[dict], args: IOArguments, writer: MongoStreamer, writer2: FileStreamer, item_is_batch: bool = True):
+def filter_many(item: dict | Iterable[dict], writer: FileStreamer, item_is_batch: bool = True):
     batch = item if item_is_batch else [item]
     rows = [filter_one(x) for x in batch]
-    rows = [row.to_dict() for row in rows if row]
-    if len(rows) > 0:
-        writer.table.insert_many(rows)
-
-
-def filter_many2(item: dict | Iterable[dict], args: IOArguments, writer: MongoStreamer, writer2: FileStreamer, item_is_batch: bool = True):
-    batch = item if item_is_batch else [item]
-    with ProcessPoolExecutor(max_workers=args.env.max_workers) as exe:
-        jobs = [exe.submit(filter_one, x) for x in batch]
-        rows = [job.result(timeout=args.env.waiting_sec) for job in jobs]
-    rows = [row.to_dict() for row in rows if row]
-    if len(rows) > 0:
-        writer.table.insert_many(rows)
-
-
-def filter_many3(item: dict | Iterable[dict], args: IOArguments, writer: MongoStreamer, writer2: FileStreamer, item_is_batch: bool = True):
-    batch = item if item_is_batch else [item]
-    with multiprocessing.Pool(processes=args.env.max_workers) as pool:
-        jobs = [pool.apply_async(filter_one, (x,)) for x in batch]
-        rows = [job.get(timeout=args.env.waiting_sec) for job in jobs]
-    rows = [row.to_dict() for row in rows if row]
-    if len(rows) > 0:
-        writer.table.insert_many(rows)
+    for row in rows:
+        if row:
+            writer.fp.write(row)
 
 
 @app.command()
@@ -68,9 +40,9 @@ def filter(
         debugging: bool = typer.Option(default=False),
         # input
         input_start: int = typer.Option(default=0),
-        input_limit: int = typer.Option(default=-1),
-        input_batch: int = typer.Option(default=1000),
-        input_inter: int = typer.Option(default=5000),
+        input_limit: int = typer.Option(default=200000),
+        input_batch: int = typer.Option(default=100),
+        input_inter: int = typer.Option(default=10000),
         input_total: int = typer.Option(default=113850250),  # 112473850 vs. 113850250 # https://www.wikidata.org/wiki/Wikidata:Statistics  # TODO: Replace with (actual count)
         input_table_home: str = typer.Option(default="localhost:8800/wikidata"),
         input_table_name: str = typer.Option(default="wikidata-20240916-parse"),
@@ -78,11 +50,6 @@ def filter(
         output_file_home: str = typer.Option(default="output/wikidata"),
         output_file_name: str = typer.Option(default="wikidata-20240916-filter.jsonl"),
         output_file_mode: str = typer.Option(default="w"),
-        output_table_home: str = typer.Option(default="localhost:8800/wikidata"),
-        output_table_name: str = typer.Option(default="wikidata-20240916-filter"),
-        output_table_reset: bool = typer.Option(default=False),
-        # option
-        processor: str = typer.Option(default="filter_many1"),
 ):
     env = ProjectEnv(
         project=project,
@@ -113,49 +80,30 @@ def filter(
             mode=output_file_mode,
             required=True,
         ),
-        table=TableOption(
-            home=output_table_home,
-            name=output_table_name,
-            reset=output_table_reset,
-            required=True,
-        )
-    )
-    extra_opt = ExtraOption(
-        processor=processor if env.max_workers > 1 else "filter_many1",
     )
     args = IOArguments(
         env=env,
         input=input_opt,
         output=output_opt,
-        option=extra_opt,
     )
     tqdm = mute_tqdm_cls()
     assert args.input.table, "input.table is required"
     assert args.output.file, "output.file is required"
-    assert args.output.table, "output.table is required"
 
     with (
         JobTimer(f"python {args.env.current_file} {' '.join(args.env.command_args)}", args=args, rt=1, rb=1, rc='='),
         MongoStreamer(args.input.table) as input_table,
         FileStreamer(args.output.file) as output_file,
-        MongoStreamer(args.output.table) as output_table,
     ):
         # filter korean wikipedia entities
         input_data = args.input.ready_inputs(input_table, total=len(input_table))
-        logger.info(f"Filter from [{input_table.opt}] to [{output_table.opt}]")
+        logger.info(f"Filter from [{input_table.opt}] to [{output_file.opt}]")
         logger.info(f"- [input] total={args.input.total} | start={args.input.start} | limit={args.input.limit}"
                     f" | {type(input_data).__name__}={input_data.num_item}{f'x{args.input.batch}ea' if input_data.has_batch_items() else ''}")
-        logger.info(f"- [output] table.reset={args.output.table.reset} | table.timeout={args.output.table.timeout}")
+        logger.info(f"- [output] file.reset={args.output.file.reset} | file.mode={args.output.file.mode}")
         with tqdm(total=input_data.num_item, unit="item", pre="=>", desc="filtering", unit_divisor=math.ceil(args.input.inter / args.input.batch)) as prog:
             for item in input_data.items:
-                if args.option.processor == "filter_many1":
-                    filter_many1(item=item, args=args, writer=output_table, item_is_batch=input_data.has_batch_items())
-                elif args.option.processor == "filter_many2":
-                    filter_many2(item=item, args=args, writer=output_table, item_is_batch=input_data.has_batch_items())
-                elif args.option.processor == "filter_many3":
-                    filter_many3(item=item, args=args, writer=output_table, item_is_batch=input_data.has_batch_items())
-                else:
-                    assert False, f"Unknown processor: {args.option.processor}"
+                filter_many(item=item, writer=output_file, item_is_batch=input_data.has_batch_items())
                 prog.update()
                 if prog.n == prog.total or prog.n % prog.unit_divisor == 0:
                     logger.info(prog)
