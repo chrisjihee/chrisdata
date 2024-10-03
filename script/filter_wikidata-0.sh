3c25212d6d754836bed8102c559b76fcb97803a9
Jihee Ryu <chrisjihee@etri.re.kr>
2024-10-03 21:00:33 +0900

    Update filter_wikidata.sh

--- script/filter_wikidata-0.sh
+++
@@ -1,5 +0,0 @@
-python -m chrisdata.cli wikidata filter --output-table-reset
-python -m chrisdata.cli wikidata filter --input-limit 30000000 --output-table-reset
-python -m chrisdata.cli wikidata filter --input-start 30000000 --input-limit 30000000
-python -m chrisdata.cli wikidata filter --input-start 60000000 --input-limit 30000000
-python -m chrisdata.cli wikidata filter --input-start 90000000
