d383c144f7626050d9c708f4b6ff3d4f2bb29b9d
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-20 01:52:14 +0900

    Add help.sh

---
+++ scripts/help.sh
@@ -0,0 +1,5 @@
+python -m chrisdata.cli --help
+python -m chrisdata.cli net --help
+python -m chrisdata.cli ner --help
+python -m chrisdata.cli wikidata --help
+python -m chrisdata.cli wikipedia --help
