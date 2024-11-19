f41472842e9a0147f1afcd190f035ffde0815f76
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-20 01:58:30 +0900

    Add ner_crawl_wiki.sh

--- scripts/help.sh
+++ scripts/help.sh
@@ -1,5 +1,9 @@
 python -m chrisdata.cli --help
-python -m chrisdata.cli net --help
+#python -m chrisdata.cli net --help
 python -m chrisdata.cli ner --help
-python -m chrisdata.cli wikidata --help
-python -m chrisdata.cli wikipedia --help
+#python -m chrisdata.cli wikidata --help
+#python -m chrisdata.cli wikipedia --help
+python -m chrisdata.cli ner crawl_wiki --help
+python -m chrisdata.cli ner convert_conll --help
+python -m chrisdata.cli ner convert_wiki --help
+python -m chrisdata.cli ner convert_json --help
