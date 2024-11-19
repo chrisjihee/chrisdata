4aae8a40837362207bb67b6c6d38d9f7fc177ecb
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-20 07:35:09 +0900

    Update ner_crawl_wiki.sh

--- scripts/ner_crawl_wiki.sh
+++ scripts/ner_crawl_wiki.sh
@@ -1,4 +1,4 @@
-python -m chrisdata.cli ner crawl_wiki GNER/data/pile-ner.json output/GNER/wiki_passage_from_pile.jsonl
-python -m chrisdata.cli ner crawl_wiki GNER/data/zero-shot-train.jsonl output/GNER/wiki_passage_from_zero-train.jsonl
-python -m chrisdata.cli ner crawl_wiki GNER/data/zero-shot-test.jsonl output/GNER/wiki_passage_from_zero-test.jsonl
+#python -m chrisdata.cli ner crawl_wiki GNER/data/pile-ner.json output/GNER/wiki_passage_from_pile.jsonl
+#python -m chrisdata.cli ner crawl_wiki GNER/data/zero-shot-train.jsonl output/GNER/wiki_passage_from_zero-train.jsonl
+#python -m chrisdata.cli ner crawl_wiki GNER/data/zero-shot-test.jsonl output/GNER/wiki_passage_from_zero-test.jsonl
 python -m chrisdata.cli ner crawl_wiki GNER/data/zero-shot-dev.jsonl output/GNER/wiki_passage_from_zero-dev.jsonl
