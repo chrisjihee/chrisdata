dd3c4897f65445019e4dee9fbcd1d94697396022
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-20 02:10:25 +0900

    Add ner_convert_wiki.sh

---
+++ scripts/ner_convert_wiki.sh
@@ -0,0 +1,8 @@
+python -m chrisdata.cli ner convert_wiki output/GNER/wiki_passage_from_pile.jsonl \
+                                         GNER/data/linked-entity-pile.jsonl --split-name train
+
+python -m chrisdata.cli ner convert_wiki output/GNER/wiki_passage_from_zero-train.jsonl \
+                                         GNER/data/linked-entity-zero-train.jsonl --split-name train
+
+python -m chrisdata.cli ner convert_wiki output/GNER/wiki_passage_from_zero-test.jsonl \
+                                         GNER/data/linked-entity-zero-test.jsonl --split-name test
