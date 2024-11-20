85875594dbca37c045f12b808d9944c3c3ba51fa
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-20 09:01:45 +0900

    Update ner_convert_wiki.sh and save

--- scripts/ner_convert_wiki.sh
+++ scripts/ner_convert_wiki.sh
@@ -6,3 +6,6 @@ python -m chrisdata.cli ner convert_wiki output/GNER/wiki_passage_from_zero-trai
 
 python -m chrisdata.cli ner convert_wiki output/GNER/wiki_passage_from_zero-test.jsonl \
                                          GNER/data/linked-entity-zero-test.jsonl --split-name test
+
+python -m chrisdata.cli ner convert_wiki output/GNER/wiki_passage_from_zero-dev.jsonl \
+                                         GNER/data/linked-entity-zero-dev.jsonl --split-name validation
