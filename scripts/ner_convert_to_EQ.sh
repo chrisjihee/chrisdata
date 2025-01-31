799a3546d1cb4d6f38a6c06d036d707679088614
Jihee Ryu <chrisjihee@etri.re.kr>
2025-01-31 18:33:30 +0900

    Save ner_convert_to_EQ.sh

---
+++ scripts/ner_convert_to_EQ.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# List of datasets
+datasets=(
+  "crossner_ai"
+  "crossner_literature"
+  "crossner_music"
+  "crossner_politics"
+  "crossner_science"
+  "mit-movie"
+  "mit-restaurant"
+)
+
+# List of splits
+splits=("train" "test" "dev")
+
+# Iterate over each dataset and split
+for dataset in "${datasets[@]}"; do
+  for split in "${splits[@]}"; do
+    python -m chrisdata.cli ner convert_to_EQ "data/gner/each/$dataset-$split.jsonl"
+  done
+done
