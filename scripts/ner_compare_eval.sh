1637a0b7e2cdfd556c4bca5a96726666b6bd8ab8
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-17 03:35:45 +0900

    Remove old scripts

--- scripts/ner_compare_eval.sh
+++
@@ -1,7 +0,0 @@
-#python -m chrisdata.cli ner compare_eval GNER/output/flan-t5-base-task-adaptation-12ep.out \
-#                                         GNER/output/flan-t5-base-concept-learning-24ep.out \
-#                                         GNER/output/flan-t5-base-comparison.xlsx
-
-python -m chrisdata.cli ner compare_eval GNER/output/llama3_1b_supervised-base.out \
-                                         GNER/output/llama3_1b_supervised-plus.out \
-                                         GNER/output/llama3_1b_supervised.xlsx
