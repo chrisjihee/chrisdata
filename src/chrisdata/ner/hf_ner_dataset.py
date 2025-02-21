e9f2c21d21baf9706bdae1fe32081702f88598d9
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-22 02:15:28 +0900

    Save data/wikiann-en

--- src/chrisdata/ner/hf_ner_dataset.py
+++ src/chrisdata/ner/hf_ner_dataset.py
@@ -244,11 +244,18 @@ if __name__ == "__main__":
         # https://huggingface.co/datasets/tner/ontonotes5
         # HfNerDatasetInfo(id="ontonotes5", hf_name="tner/ontonotes5", label_column="tags", label2id=ontonotes5_label2id),
 
+        # https://huggingface.co/datasets/rmyeid/polyglot_ner
+        # HfNerDatasetInfo(id="polyglot_ner", hf_name="rmyeid/polyglot_ner"),  # FileNotFoundError: http://cs.stonybrook.edu/~polyglot/ner2/emnlp_datasets.tgz
+
         # https://huggingface.co/datasets/tner/tweetner7
         # HfNerDatasetInfo(id="tweetner7", hf_name="tner/tweetner7", label_column="tags",
         #                  train_splits=["train_2020", "train_2021"],
         #                  dev_splits=["validation_2020", "validation_2021"],
         #                  test_splits=["test_2020"]),
+
+        # https://huggingface.co/datasets/unimelb-nlp/wikiann
+        HfNerDatasetInfo(id="wikiann-en", hf_name="unimelb-nlp/wikiann", subset="en"),
+
     ]
     for dataset_info in dataset_infos:
         download_hf_dataset(dataset_info)
@@ -257,11 +264,6 @@ if __name__ == "__main__":
     #     "Babelscape/wikineural", "data/WikiNeural-en", label2id=wikineural_label2id,
     #     train_split="train_en", dev_splits=("val_en",), test_splits=("test_en",),
     # )
-    # download_hf_dataset(
-    #     dataset_path="unimelb-nlp/wikiann::en",
-    #     output_dir="data/WikiANN-en",
-    # )
-    # download_hf_dataset("rmyeid/polyglot_ner", "data/PolyglotNER")
 
     # for dataset_dir in sorted([x for x in Path("data").glob("*") if x.is_dir()]):
     #     if not Path(dataset_dir / "label.txt").exists():
