013216c63fa1563e2e7f74a4a75f6566a5f40c6d
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-01 10:39:15 +0900

    Update chrisdata/scripts/ner_convert_to_WQ.py

--- scripts/ner_convert_to_WQ.py
+++ scripts/ner_convert_to_WQ.py
@@ -1,37 +1,14 @@
 import subprocess
 
-# List of datasets
-datasets = [
-    "crossner_ai",
-    "crossner_literature",
-    "crossner_music",
-    "crossner_politics",
-    "crossner_science",
-    "mit-movie",
-    "mit-restaurant",
-]
 target_label_levels = ["1", "3", "5"]
+data_files = [
+    "data/pile-ner.jsonl",
+    "data/ZSE-test.jsonl",
+    "data/ZSE-validation.jsonl",
+]
 
 # Iterate over each dataset and split
-for dataset in datasets:
-    for label_levels in target_label_levels:
-        for split in ["train", "test", "dev"]:
-            input_path_1 = f"data/gner/each-BL/{dataset}-{split}.jsonl"
-            input_path_2 = f"data/gner/each-sampled-BL/{dataset}-{split}=100.jsonl"
-            subprocess.run((f"python -m chrisdata.cli ner convert_to_WQ {input_path_1}"
-                            f" --label_level_main {label_levels}").split())
-            subprocess.run((f"python -m chrisdata.cli ner convert_to_WQ {input_path_2}"
-                            f" --label_level_main {label_levels}").split())
-
 for label_levels in target_label_levels:
-    for input_path in [
-        "data/gner/united/pile-ner.jsonl",
-        "data/gner/united/zero-shot-dev.jsonl",
-        "data/gner/united/zero-shot-dev-100.jsonl",
-        "data/gner/united/zero-shot-dev-200.jsonl",
-        "data/gner/united/zero-shot-test.jsonl",
-        "data/gner/united/zero-shot-test-100.jsonl",
-        "data/gner/united/zero-shot-test-200.jsonl",
-    ]:
-        subprocess.run((f"python -m chrisdata.cli ner convert_to_WQ {input_path}"
+    for data_file in data_files:
+        subprocess.run((f"python -m chrisdata.cli ner convert_to_WQ {data_file}"
                         f" --label_level_main {label_levels}").split())
