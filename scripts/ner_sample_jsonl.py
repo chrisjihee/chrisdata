3996f7499dae50b4e6e3afa6178b25d46f68bef4
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-14 23:44:29 +0900

    Update minor

--- scripts/ner_sample_jsonl.py
+++ scripts/ner_sample_jsonl.py
@@ -3,13 +3,13 @@ from chrisdata.ner.gner import stratified_sample_jsonl_lines
 stratified_sample_jsonl_lines(
     input_file="data/pile-ner.jsonl",
     min_num_word=10, max_num_word=100,
-    min_num_label=3, max_num_label=10,
+    min_num_label=3, max_num_label=7,
     min_num_samples=3, max_num_samples=10,
 )  # 19,988
 stratified_sample_jsonl_lines(
     input_file="data/pile-ner.jsonl",
     min_num_word=10, max_num_word=100,
-    min_num_label=3, max_num_label=7,
+    min_num_label=3, max_num_label=10,
     min_num_samples=3, max_num_samples=10,
 )  # 30,129
 stratified_sample_jsonl_lines(
