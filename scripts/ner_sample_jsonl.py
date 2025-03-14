e28d4c28b9f49d21f4bda87ea59894d8539eefdf
Jihee Ryu <chrisjihee@etri.re.kr>
2025-03-14 23:37:32 +0900

    Update ner_sample_jsonl.py: sampling train, dev, test set

--- scripts/ner_sample_jsonl.py
+++ scripts/ner_sample_jsonl.py
@@ -1,10 +1,48 @@
 from chrisdata.ner.gner import stratified_sample_jsonl_lines
 
-# stratified_sample_jsonl_lines(input_file="data/pile-ner.jsonl", min_num_word=7, max_num_word=120, min_num_label=3, max_num_label=20, min_num_samples=3, max_num_samples=10)
-# stratified_sample_jsonl_lines(input_file="data/pile-ner.jsonl", min_num_word=10, max_num_word=100, min_num_label=3, max_num_label=20, min_num_samples=3, max_num_samples=10)
-# stratified_sample_jsonl_lines(input_file="data/pile-ner.jsonl", min_num_word=10, max_num_word=100, min_num_label=3, max_num_label=10, min_num_samples=3, max_num_samples=10)
-# stratified_sample_jsonl_lines(input_file="data/pile-ner.jsonl", min_num_word=10, max_num_word=100, min_num_label=3, max_num_label=7, min_num_samples=3, max_num_samples=10)
-stratified_sample_jsonl_lines(input_file="data/ZSE-validation.jsonl", max_num_samples=100,
-                              output_file="data/ZSE-validation-sampled.jsonl")
-stratified_sample_jsonl_lines(input_file="data/ZSE-test.jsonl", max_num_samples=100,
-                              output_file="data/ZSE-test-sampled.jsonl")
+stratified_sample_jsonl_lines(
+    input_file="data/pile-ner.jsonl",
+    min_num_word=10, max_num_word=100,
+    min_num_label=3, max_num_label=10,
+    min_num_samples=3, max_num_samples=10,
+)  # 19,988
+stratified_sample_jsonl_lines(
+    input_file="data/pile-ner.jsonl",
+    min_num_word=10, max_num_word=100,
+    min_num_label=3, max_num_label=7,
+    min_num_samples=3, max_num_samples=10,
+)  # 30,129
+stratified_sample_jsonl_lines(
+    input_file="data/pile-ner.jsonl",
+    min_num_word=10, max_num_word=100,
+    min_num_label=3, max_num_label=20,
+    min_num_samples=3, max_num_samples=10,
+)  # 39,852
+stratified_sample_jsonl_lines(
+    input_file="data/pile-ner.jsonl",
+    min_num_word=7, max_num_word=120,
+    min_num_label=3, max_num_label=20,
+    min_num_samples=3, max_num_samples=10,
+)  # 49,800
+
+stratified_sample_jsonl_lines(
+    input_file="data/ZSE-validation.jsonl",
+    output_file="data/ZSE-validation-sampled.jsonl",
+    max_num_samples=10,
+)  # 70
+stratified_sample_jsonl_lines(
+    input_file="data/ZSE-validation.jsonl",
+    output_file="data/ZSE-validation-sampled.jsonl",
+    max_num_samples=100,
+)  # 700
+
+stratified_sample_jsonl_lines(
+    input_file="data/ZSE-test.jsonl",
+    output_file="data/ZSE-test-sampled.jsonl",
+    max_num_samples=100,
+)  # 700
+stratified_sample_jsonl_lines(
+    input_file="data/ZSE-test.jsonl",
+    output_file="data/ZSE-test-sampled.jsonl",
+    max_num_samples=300,
+)  # 2,100
