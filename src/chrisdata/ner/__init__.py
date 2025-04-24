29dd2bbb138b203838c083ddf06995c719f8275d
Jihee Ryu <chrisjihee@etri.re.kr>
2025-04-25 00:52:45 +0900

    Update set_instruction_prompt(): shuffle = False

--- src/chrisdata/ner/__init__.py
+++ src/chrisdata/ner/__init__.py
@@ -119,7 +119,7 @@ class GenNERSample(GenSeq2SeqSample):
 
         self.prompt_labels = GNERDataset._generate_labeled_string(words, labels)
 
-    def set_instruction_prompt(self, instruction_file: Path | str, label_list: list[str], shuffle: bool = True):
+    def set_instruction_prompt(self, instruction_file: Path | str, label_list: list[str], shuffle: bool = False):
         words = self.words
         labels = self.labels
 
@@ -127,11 +127,10 @@ class GenNERSample(GenSeq2SeqSample):
         instruction_file = Path(instruction_file)
         if instruction_file.suffix == ".json":
             dataset.config = GNERConfig(instruction_file=instruction_file)
+            instruction = dataset._get_instruction()
         else:
-            dataset.config = GNERConfig()
-            dataset.config.instructions = [instruction_file.read_text()]
+            instruction = instruction_file.read_text()
 
-        instruction = dataset._get_instruction()
         if shuffle:
             random.shuffle(label_list)
         instruction += f"\nUse the specific entity tags: {', '.join(label_list)} and O.\n"
